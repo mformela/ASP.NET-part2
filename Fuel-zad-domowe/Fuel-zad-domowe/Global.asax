21486961bc64ce345d2529c2aeb6ccafb4d2e1be
Marta Formela <formela.marta@gmail.com>
2017-11-18 09:34:09 +0100

    Initial commit

---
+++ Fuel-zad-domowe/Fuel-zad-domowe/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Fuel_zad_domowe.MvcApplication" Language="C#" %>
---
+++ Fuel-zad-domowe/Fuel-zad-domowe/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Fuel_zad_domowe.Startup))]
+namespace Fuel_zad_domowe
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
