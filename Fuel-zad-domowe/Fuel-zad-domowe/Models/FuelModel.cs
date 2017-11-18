fc1402679b7e845916cf8f8b670c6414470ecbaa
Marta Formela <formela.marta@gmail.com>
2017-11-18 09:48:08 +0100

    dodanie modeli

---
+++ Fuel-zad-domowe/Fuel-zad-domowe/Models/FuelModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Fuel_zad_domowe.Models
+{
+    public class FuelModel
+    {
+        public int Fuel { get; set; }
+        public int Distance { get; set; }
+        public double AvgConsumption { get; set; }
+    }
+}
